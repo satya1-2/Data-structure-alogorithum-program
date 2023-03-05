8d4603ace3e2c436119fd45153f35551403e8b7b
satya <satya143@gmail.com>
2023-03-05 10:40:21 +0530

    check prime number between 0 to 1000 range

--- algorithmProgram/AlgorithmProgram.java
+++ algorithmProgram/AlgorithmProgram.java
@@ -1,35 +1,89 @@
 package algorithmProgram;
 
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class AlgorithmProgram {
+    boolean isPrime(int n) {
 
-    static boolean areAnagram(char[] str1, char[] str2)
-    {
-        int n1 = str1.length;
-        int n2 = str2.length;
+        int c = 0;
 
-        if (n1 != n2)
-            return false;
-        Arrays.sort(str1);
-        Arrays.sort(str2);
-        for (int i = 0; i < n1; i++)
-            if (str1[i] != str2[i])
-                return false;
+        for (int i = 1; i <= n; i++) {
 
-        return true;
-    }
-    public static void main(String args[])
-    {
-        char string1[] = { 'g', 'r', 'a', 'm' };
-        char string2[] = { 'a', 'r', 'm' };
+            if (n % i == 0)
 
-        // Function Call
-        if (areAnagram(string1, string2))
-            System.out.println("The two strings are"
-                    + " anagram of each other");
+                c++;
+
+        }
+
+        if (c == 2)
+
+            return true;
+
         else
-            System.out.println("The two strings are not"
-                    + " anagram of each other");
+
+            return false;
+
     }
+
+    public static void main(String args[]) throws IOException {
+        AlgorithmProgram ob = new AlgorithmProgram();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter the number of rows : ");
+
+        int m = Integer.parseInt(br.readLine());
+
+        System.out.print("Enter the number of columns : ");
+
+        int n = Integer.parseInt(br.readLine());
+        int A[][] = new int[m][n];
+        int B[] = new int[m * n];
+        int i = 0, j;
+        int k = 1;
+
+
+        while (i < m * n) {
+
+            if (ob.isPrime(k) == true) {
+
+                B[i] = k;
+
+                i++;
+
+            }
+
+            k++;
+
+        }
+        int x = 0;
+
+        for (i = 0; i < m; i++) {
+
+            for (j = 0; j < n; j++) {
+
+                A[i][j] = B[x];
+
+                x++;
+
+            }
+
+        }
+        System.out.println("The Final Array is : ");
+
+        for (i = 0; i < m; i++) {
+
+            for (j = 0; j < n; j++) {
+
+                System.out.print(A[i][j] + "\t");
+
+            }
+
+            System.out.println();
+
+        }
+
+    }
+
 }
