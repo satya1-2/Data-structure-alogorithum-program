9f74e2f839e61827c76c5a43930e311911f6dc7b
satya <satya143@gmail.com>
2023-03-05 10:15:18 +0530

    Added :two  Anagrams String  are eachother or not

--- algorithmProgram/AlgorithmProgram.java
+++ algorithmProgram/AlgorithmProgram.java
@@ -1,80 +1,35 @@
 package algorithmProgram;
 
+import java.util.Arrays;
+
 public class AlgorithmProgram {
 
-    void mergeSort(int arr[], int l, int m, int r) {
-        // Find sizes of two subArrays to be merged
-        int n1 = m - l + 1;
-        int n2 = r - m;
+    static boolean areAnagram(char[] str1, char[] str2)
+    {
+        int n1 = str1.length;
+        int n2 = str2.length;
 
-        /* Create temp arrays */
-        int Left[] = new int[n1];
-        int Right[] = new int[n2];
+        if (n1 != n2)
+            return false;
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+        for (int i = 0; i < n1; i++)
+            if (str1[i] != str2[i])
+                return false;
 
-        for (int i = 0; i < n1; ++i)
-            Left[i] = arr[l + i];
-        for (int j = 0; j < n2; ++j)
-            Right[j] = arr[m + 1 + j];
-
-        // Initial indexes of first and second subArrays
-        int i = 0, j = 0;
-
-        int k = l;
-        while (i < n1 && j < n2) {
-            if (Left[i] <= Right[j]) {
-                arr[k] = Left[i];
-                i++;
-            } else {
-                arr[k] = Right[j];
-                j++;
-            }
-            k++;
-        }
-        while (i < n1) {
-            arr[k] = Left[i];
-            i++;
-            k++;
-        }
-
-        while (j < n2) {
-            arr[k] = Right[j];
-            j++;
-            k++;
-        }
+        return true;
     }
+    public static void main(String args[])
+    {
+        char string1[] = { 'g', 'r', 'a', 'm' };
+        char string2[] = { 'a', 'r', 'm' };
 
-    void sort(int array[], int l, int r) {
-        if (l < r) {
-            // Find the middle point
-            int m = l + (r - l) / 2;
-
-            // Sort first and second halves
-            sort(array, l, m);
-            sort(array, m + 1, r);
-
-            // Merge the sorted halves
-            mergeSort(array, l, m, r);
-        }
-    }
-
-    static void printArray(int array[]) {
-        int n = array.length;
-        for (int i = 0; i < n; ++i)
-            System.out.print(array[i] + " ");
-        System.out.println();
-    }
-
-    public static void main(String args[]) {
-        int array[] = {12, 11, 13, 5, 6, 7, 23, 45, 46};
-
-        System.out.println("Given Array");
-        printArray(array);
-
-        AlgorithmProgram ob = new AlgorithmProgram();
-        ob.sort(array, 0, array.length - 1);
-
-        System.out.println("\nSorted array");
-        printArray(array);
+        // Function Call
+        if (areAnagram(string1, string2))
+            System.out.println("The two strings are"
+                    + " anagram of each other");
+        else
+            System.out.println("The two strings are not"
+                    + " anagram of each other");
     }
 }
-
