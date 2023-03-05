fd5e29b2f6fe2a67467f1e212e9c636d4d3bb341
satya <satya143@gmail.com>
2023-03-05 08:56:49 +0530

    Added : welcome to data structure solutions

---
+++ algorithmProgram/AlgorithmProgram.java
@@ -0,0 +1,7 @@
+package algorithmProgram;
+
+public class AlgorithmProgram {
+    public static void main(String[] args) {
+        System.out.println("welcome to data Structure algorithm program solution : ");
+    }
+}
